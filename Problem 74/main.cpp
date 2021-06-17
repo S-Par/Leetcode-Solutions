7f73319d548221dc7a915055815c4219166182a8
sidpar <sidpar@umich.edu>
2021-06-17 23:01:39 +0530

    Adapted solution of 240 to solve Problem 74

---
+++ Problem 74/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <cassert>
+using namespace std;
+
+// Question: Write an efficient algorithm that searches for a value in an m x n matrix. 
+// This matrix has the following properties: 
+// Integers in each row are sorted in ascending from left to right
+// The first integer of each row is greater than the last integer of the previous row.
+bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    // Linear stepwise search: (movements until you hit target)
+    // Start at the bottom-left, move right until you hit a value greater than the target
+    // then move up until you reach a value lower than the target then right again
+    // If you hit the right edge without finding target, return false
+    int m = matrix.size(), n = matrix[0].size();
+    int row = m - 1, col = 0;
+    while (row >= 0 && col < n) {
+        if (matrix[row][col] == target) {
+            return true;
+        }
+        else if (matrix[row][col] > target) {
+            --row;
+        }
+        else {
+            ++col;
+        }
+    }
+
+    // Didn't find target, return false
+    return false;
+}
+
+int main() {
+    vector<vector <int>> matrix = {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
+    assert(searchMatrix(matrix, 3));
+    assert(searchMatrix(matrix, 16));
+    assert(searchMatrix(matrix, 60));
+    assert(!searchMatrix(matrix, 2));
+    assert(searchMatrix(matrix, 7));
+    assert(!searchMatrix(matrix, 8));
+    cout << "PASS!" << endl;
+    return 0;
+}
